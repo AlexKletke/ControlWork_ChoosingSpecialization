9a05312b87c19015d03e13fbd8bf31f1170592fc
AlexandrKletke <klete@mail.ru>
2022-12-16 15:06:50 +0300

    added the example solution code

--- In_C#/Program.cs
+++ In_C#/Program.cs
@@ -9,3 +9,34 @@ string words = "";
 //words = Array[1].ToString() + Array[0].ToString();
 //Massive = Array[0].ToString() + words;
 //WriteLine(Massive.ToString());
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i] == ' ' || Array[i] == ',' || Array[i] == '.' || Array[i] == '!' || Array[i] == ';' || Array[i] == '?')
+    {
+        if (count > 3)
+        {
+            count = 0;
+            words = String.Empty;
+        }
+        else
+        {
+            Massive = Massive + words + Array[i];
+            count = 0;
+            words = String.Empty;
+        }
+    }
+    else
+    {
+        words = words + Array[i];
+        count++;
+    }
+}
+if (count > 3)
+{
+    WriteLine(Massive);
+}
+else
+{
+    Massive = Massive + words;
+    WriteLine(Massive);
+}
